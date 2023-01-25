2158a6035edf1ba0ea95dfa3b75f80cefdf04a5e
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2023-01-25 22:11:29 +0800

    Change path test to tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,13 @@
 import type { JestConfigWithTsJest } from 'ts-jest'
 
 const jestConfig: JestConfigWithTsJest = {
-  roots: ["<rootDir>./src/test/"],
+  roots: ["<rootDir>./src/tests/"],
   testMatch: ["**/*.test.ts"],
   clearMocks: true,
   coverageProvider: "v8",
   preset: "ts-jest/presets/js-with-ts",
-  setupFilesAfterEnv: ["./src/test/setup.ts"],
-  collectCoverageFrom: ["./src/test/*.ts"],
+  setupFilesAfterEnv: ["./src/tests/setup.ts"],
+  collectCoverageFrom: ["./src/tests/*.ts"],
   transform: {
     // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
     // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
